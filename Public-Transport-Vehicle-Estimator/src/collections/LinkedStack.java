9f9a6028935e596355429ac4e07eb728713ab391
Isaac Gaiovnik <39385371+IsGai@users.noreply.github.com>
2018-11-24 18:08:33 -0600

    Revert "Updated collections."
    This reverts commit 9d85f5eca3ae07447d1beaaead06c407db285f05.

--- Public-Transport-Vehicle-Estimator/src/collections/LinkedStack.java
+++ Public-Transport-Vehicle-Estimator/src/collections/LinkedStack.java
@@ -1,47 +1,61 @@
 package collections;
-
-
+//File: LinkedStack.java from the package edu.colorado.collections
+//Complete documentation is available from the LinkedStack link in:
+//http://www.cs.colorado.edu/~main/docs/
 
 import java.util.EmptyStackException;
 
-
+/******************************************************************************
+* A <CODE>LinkedStack</CODE> is a stack of references to
+* <code>E</code>objects.
+*
+* <b>Limitations:</b>
+*   Beyond <CODE>Int.MAX_VALUE</CODE> items, <CODE>size</CODE> is wrong. 
+*
+* <b>Java Source Code for this class:</b>
+*   <A HREF="../../../../edu/colorado/collections/LinkedStack.java">
+*   http://www.cs.colorado.edu/~main/edu/colorado/collections/LinkedStack.java
+*   </A>
+*
+* @author Michael Main 
+*   <A HREF="mailto:main@colorado.edu"> (main@colorado.edu) </A>
+*
+* @version Feb 10, 2016
+*
+* @see ArrayStack
+******************************************************************************/
 public class LinkedStack<E> implements Cloneable
 {
+// Invariant of the LinkedStack class:
+//   1. The items in the stack are stored in a linked list, with the top of
+//      the stack stored at the head node, down to the bottom of the stack
+//      at the final node.
+//   2. The instance variable top is the head reference of the linked list
+//      of items.
+private Node<E> top; 
 
-private Node<E> head; 
-
-/*
- * Description: Constructor for the class.
- * 
- * Arguments: None.
- * 
- * Precondition: Type used in place of generic should be cloneable.
- * 
- * Postcondition: An object of type LinkedStack will be created.
- * 
- * Throws: None.
- */   
+/**
+* Initialize an empty stack.
+* <b>Postcondition:</b>
+*   This stack is empty.
+**/   
 public LinkedStack( )
 {
-   head = null;
+   top = null;
 }
 
 
-/*
- * Description: Clones the calling object.
- * 
- * Arguments: None.
- * 
- * Precondition: Object type used in place of generic should be cloneable.
- * 
- * Postcondition: A clone of the called LinkedStack object will be returned.
- * 
- * Throws: Runtime exception - if cloneable was not implemented
- *  in the typed being used in the place of generic.
- */
+/**
+* Generate a copy of this stack.
+* @return
+*   The return value is a copy of this stack. Subsequent changes to the
+*   copy will not affect the original, nor vice versa.
+* @exception OutOfMemoryError
+*   Indicates insufficient memory for creating the clone.
+**/ 
 @SuppressWarnings("unchecked")
 public LinkedStack<E> clone( )       
-{  
+{  // Clone a LinkedStack<E>.
    LinkedStack<E> answer;
    
    try
@@ -50,102 +64,100 @@ public LinkedStack<E> clone( )
    }
    catch (CloneNotSupportedException e)
    { 
-    
+      // This exception should not occur. But if it does, it would probably indicate a
+      // programming error that made super.clone unavailable. The most comon error
+      // The most common error would be forgetting the "Implements Cloneable"
+      // clause at the start of this class.
       throw new RuntimeException
       ("This class does not implement Cloneable");
   }
    
-  
-   answer.head = Node.listCopy(head);
+   // The generic listCopy method gets the type of E from top.
+   answer.top = Node.listCopy(top);
    
    return answer;
 }        
 
 
-/*
- * Description: Checks if the stack is empty. 
- * 
- * Arguments: None.
- * 
- * Precondition: LinkedStack object should be initialized.
- * 
- * Postcondition: If stack is empty true will be returned. If not
- * false will be returned.
- * 
- * Throws: None.
- */
+/**
+* Determine whether this stack is empty.
+* @return
+*   <CODE>true</CODE> if this stack is empty;
+*   <CODE>false</CODE> otherwise. 
+**/
 public boolean isEmpty( )
 {
-   return (head == null);
+   return (top == null);
 }
 
 
-/*
- * Description: Checks the next item without removing it.
- * 
- * Arguments: None.
- * 
- * Precondition: Stack should not be empty.
- * 
- * Postcondition: The next item will be returned.
- * 
- * Throws: EmptyStackException - if the stack is empty.
- */  
+/**
+* Get the top item of this stack, without removing the item.
+* <b>Precondition:</b>
+*   This stack is not empty.
+* @return
+*   the top item of the stack
+* @exception EmptyStackException
+*   Indicates that this stack is empty.
+**/   
 public E peek( )   
 {
-   if (head == null)
+   if (top == null)
+      // EmptyStackException is from java.util and its constructor has no argument.
       throw new EmptyStackException( );
-   return head.getData( );
+   return top.getData( );
 }
 
 
-/*
- * Description: Pops the stack.
- * 
- * Arguments: None.
- * 
- * Precondition: Stack should not be empty.
- * 
- * Postcondition: The next index will be returned and the item will be removed
- * from the stack.
- * 
- * Throws: EmptyStackException - if the stack is empty.
- */   
+/**
+* Get the top item, removing it from this stack.
+* <b>Precondition:</b>
+*   This stack is not empty.
+* @return
+*   The return value is the top item of this stack, and the item has
+*   been removed.
+* @exception EmptyStackException
+*   Indicates that this stack is empty.
+**/    
 public E pop( )
 {
    E answer;
    
-   if (head == null)    
+   if (top == null)
+      // EmptyStackException is from java.util and its constructor has no argument.
       throw new EmptyStackException( );
    
-   answer = head.getData( );
-   head = head.getLink( );
+   answer = top.getData( );
+   top = top.getLink( );
    return answer;
 }    
 
 
-/*
- * Description: Add an item to the stack.
- * 
- * Arguments: item - The item you are adding.
- * 
- * Precondition: item should be the correct type.
- * 
- * Postcondition: The item will be added to the stack.
- * 
- * Throws: None.
- */   
+/**
+* Push a new item onto this stack. The new item may be the null
+* reference.
+* @param item
+*   the item to be pushed onto this stack 
+* <b>Postcondition:</b>
+*   The item has been pushed onto this stack.
+* @exception OutOfMemoryError
+*   Indicates insufficient memory for increasing the stack's capacity.
+**/    
 public void push(E item)
 {
-   head = new Node<E>(item, head);
+   top = new Node<E>(item, top);
 }
            
 
-//Returns current size of calling object.
+/**
+* Accessor method to determine the number of items in this stack.
+* @return
+*   the number of items in this stack
+**/ 
 public int size( )   
 {
-
-   return Node.listLength(head);
+   // The generic listLength method gets the type of E from top.
+   return Node.listLength(top);
 }
 
 }
